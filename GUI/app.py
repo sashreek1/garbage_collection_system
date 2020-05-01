1e7efa7e2291fc4132b8fe63931ef5e47d8720dd
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-05-02 00:47:33 +0530

    Error fixed

--- GUI/app.py
+++ GUI/app.py
@@ -12,27 +12,56 @@ class app():
         self.app_width = int(0.5*self.width)
         self.app_length = int(0.5*self.height)
         self.obj.geometry(str(self.app_width)+"x"+str(self.app_length))
-        self.b=dustbin()
+        self.b=dustbin() #to invoke the dustbin object
         
         
-    def co_ordinate(self): 
+    def co_ordinate(self):
+        
+        def printdata(entrybox):
+            text = entrybox.get()
+            print(str(text))
+        
         cordinate = Tk()
+        
         cordinate.geometry(str(int(0.5*self.height))+"x"+str(int(0.3*self.width)))
         cordinate.title("enter the co_ordinte values")
+        
         label1 = Label(cordinate, text= "Enter the co ordinates seperated by comma" )
         label1.pack()
-        e=Entry(cordinate)
-
-        button = Button(cordinate,text='okay')
+        
+        textin = StringVar()
+        e=Entry(cordinate, width=30)
+        button = Button(cordinate,text='okay',command=lambda:printdata(e))
         button.pack(side='bottom')
-        string = e.get()
-        e.pack()
-        e.focus_set()                
-        print(string)     
+        
+        e.pack()            
         cordinate.mainloop()
+        
+    def getarea(self):
+        
+        def printdat(entrybox):
+            text = entrybox.get()
+            print(str(text))
+
+        
+        area=Tk()
+        area.geometry(str(int(0.5*self.height))+"x"+str(int(0.3*self.width)))
+        
+        label1 = Label(area, text= "Enter the Area" )
+        label1.pack()
+        
+        e=Entry(area, width=30)
+        button = Button(area,text='okay',command= lambda: printd(e))
+        button.pack(side='bottom')
+        
+        e.pack()            
+        area.mainloop()
+        
+        
 
         
     def get_info(self):
+        # this code gets the data from the Raspi
         
         sensor_data = Tk()
         sensor_data.geometry(str(int(0.5*self.height))+"x"+str(int(0.3*self.width)))
@@ -42,6 +71,8 @@ class app():
         b.update_Initial()
         current=b.current_Depth() 
         current_percentage=b.percentage_Filled()) 
+        
+        #print the data obtained from the Raspi
         
         label1 = Label(sensor_data, text= "Initial depth "+str(initial))
         label2 = Label(sensor_data, text= "intially filled % =  "+str(initial_percentage) )
@@ -54,18 +85,22 @@ class app():
         
     def start_app(self,object1):
         
+        #To start the APP
         welcome = tkFont.Font(family='arial', size=55)
-        welcome = Label(self.obj, text='*** \n App \n\n\n',font=welcome,background="green")
+        welcome = Label(self.obj, text='Garbage collection \n App \n\n\n',font=welcome,background="green")
         welcome.pack(fill='both', expand=True, anchor=CENTER)
         
         display_button = Button(object1,text="Get Data",background="white",command=self.get_info )
         display_button.place(relx=0.5, rely=0.5, anchor=CENTER)
         
-        return_button = Button(object1,text="Return Data",background="white" ) #command= from node mcu
+        return_button = Button(object1,text="Return Data",background="white" ) 
         return_button.place(relx=0.5, rely=0.6, anchor=CENTER)
         
         co_button = Button(object1,text="Enter Co-Ordinates",background="white",command=self.co_ordinate )  
         co_button.place(relx=0.5, rely=0.7, anchor=CENTER)
+        
+        area_button = Button(object1,text="Enter Area",background="white",command=self.getarea )  
+        area_button.place(relx=0.5, rely=0.8, anchor=CENTER)
  
 if __name__ == "__main__": 
     object1 = Tk()
